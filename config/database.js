6f56ec231b197f38e69164b8deef678ea48f6795
Hoang Thuan <hoangthuan171098@gmail.com>
2020-12-15 01:33:19 +0700

    'commit'

--- config/database.js
+++ config/database.js
@@ -3,13 +3,16 @@ module.exports = ({ env }) => ({
   connections: {
     default: {
       connector: 'mongoose',
+      // settings: {
+      //   host: env('DATABASE_HOST', '127.0.0.1'),
+      //   srv: env.bool('DATABASE_SRV', false),
+      //   port: env.int('DATABASE_PORT', 27017),
+      //   database: env('DATABASE_NAME', 'dclv'),
+      //   username: env('DATABASE_USERNAME', null),
+      //   password: env('DATABASE_PASSWORD', null),
+      // },
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'dclv'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        uir: "mongodb+srv://hoangthuan:Thanh171098@cluster0.hn9kj.mongodb.net/dclv?retryWrites=true&w=majority",
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
