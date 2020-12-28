5bbfb12e8239eb6996ff0d30a42f981ba72645eb
Hoang Thuan <hoangthuan171098@gmail.com>
2020-12-28 14:58:07 +0700

    2rd upload

--- config/database.js
+++ config/database.js
@@ -3,17 +3,18 @@ module.exports = ({ env }) => ({
   connections: {
     default: {
       connector: 'mongoose',
-      // settings: {
-      //   host: env('DATABASE_HOST', '127.0.0.1'),
-      //   srv: env.bool('DATABASE_SRV', false),
-      //   port: env.int('DATABASE_PORT', 27017),
-      //   database: env('DATABASE_NAME', 'dclv'),
-      //   username: env('DATABASE_USERNAME', null),
-      //   password: env('DATABASE_PASSWORD', null),
-      // },
       settings: {
-        uir: "mongodb+srv://hoangthuan:Thanh171098@cluster0.hn9kj.mongodb.net/dclv?retryWrites=true&w=majority",
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        srv: env.bool('DATABASE_SRV', false),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'dclv'),
+        username: env('DATABASE_USERNAME', null),
+        password: env('DATABASE_PASSWORD', null),
       },
+      // settings: {
+      //   uri: "mongodb://hoangthuan:Thanh171098@cluster0-shard-00-00.hn9kj.mongodb.net:27017,cluster0-shard-00-01.hn9kj.mongodb.net:27017,cluster0-shard-00-02.hn9kj.mongodb.net:27017/dclv?ssl=true&replicaSet=atlas-13rs4l-shard-0&authSource=admin&retryWrites=true&w=majority",
+      //   database: "dclv"
+      // },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
         ssl: env.bool('DATABASE_SSL', false),
